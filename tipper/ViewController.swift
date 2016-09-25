2e9208dc6f99bdc04d1cebafbc67a38434e28c14
Edwin Wong <edwinfwong@gmail.com@edwins-macbook-pro.local>
2016-09-25 00:44:21 -0700

    Added basic functionality on tipper ap

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: properties
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    private let tipPercentages = [0.15, 0.2, 0.25]
+    
+    //MARK: default methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: events
+    
+    // on screen tap, hide keyboard
+    @IBAction func onTap(_ sender: AnyObject) {
+        // force keyboard hide
+        //view.endEditing(true)
+    }
+    
+    // Update tip values when entering bill
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+    }
+    
+    
+    
 }
 
