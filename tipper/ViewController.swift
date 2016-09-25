e7929b4eb2327eff7b3200af62dc8a61ea732491
Edwin Wong <edwinfwong@gmail.com@edwins-macbook-pro.local>
2016-09-24 19:42:15 -0700

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Edwin Wong on 9/24/16.
+//  Copyright © 2016 edwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
